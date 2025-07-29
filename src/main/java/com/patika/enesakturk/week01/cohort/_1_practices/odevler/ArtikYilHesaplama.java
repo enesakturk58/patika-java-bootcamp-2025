e4379409135fcb2d0bee994efa0aa2fef282268d
Enes Aktürk <enesakturk97@gmail.com>
2025-07-29 16:53:20 +0300

    Artik yil ödevi icin gerekli yorum satirlari eklendi

--- src/main/java/com/patika/enesakturk/week01/cohort/_1_practices/odevler/ArtikYilHesaplama.java
+++ src/main/java/com/patika/enesakturk/week01/cohort/_1_practices/odevler/ArtikYilHesaplama.java
@@ -6,9 +6,11 @@ public class ArtikYilHesaplama {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        // Kullanicidan dogum yili verisi aliniyor ve yil degiskenine ataniyor.
         System.out.print("Yıl giriniz: ");
         int yil = scanner.nextInt();
 
+        // Artik yil için gerekli sartlar kontrol edilip çikti aliniyor.
         if ((yil % 4 == 0 && yil % 100 != 0) || (yil % 400 == 0)) {
             System.out.println(yil + " bir artık yıldır.");
         } else {
